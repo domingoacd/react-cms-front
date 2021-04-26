1442e56845fc92b7fe6171fcd9bf1222f23ac11f
Domingo Cabeza <domingoacd1@gmail.com>
2021-04-26 19:03:02 -0400

    Added search bar

--- src/components/formElements/InputText.jsx
+++ src/components/formElements/InputText.jsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const InputText = ({ placeholder, icon}) => {
     return (
-        <input type="text" placeholder={placeholder}/>
+        <label htmlFor="input-search" className="w-full max-w-sm flex items-center rounded-lg px-3 bg-white-tone">
+            {icon ? <img src={icon} alt="" className="w-4 mr-4"/> : "" }
+            <input id="input-search" name="input-search" type="text" placeholder={placeholder} className="max-w-sm focus:outline-none h-10 bg-transparent" />
+        </label>
     );
 }
 
